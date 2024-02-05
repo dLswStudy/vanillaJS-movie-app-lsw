efdb88dd46876d92abd41d1f23ff6541682878be
dLswStudy <devLswStudy@gmail.com>
2024-02-05 13:00:38 +0900

    영화목록 상태관리, MovieList 컴포넌트, 더보기 버튼

---
+++ src/store/movie.js
@@ -0,0 +1,24 @@
+import {Store} from "../core/heropy";
+
+
+const store = new Store({
+    searchText: '',
+    page: 1,
+    pageMax: 1,
+    movies: []
+})
+
+export {store as movieStore};
+export const searchMovies = async page => {
+    store.state.page = page
+    if (page === 1) {
+        store.state.movies = []
+    }
+    const res = await fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}`);
+    const {Search, totalResults } = await res.json();
+    store.state.movies = [
+        ...store.state.movies,
+        ...Search
+    ]
+    store.state.pageMax = Math.ceil(Number(totalResults) / 10)
+}
