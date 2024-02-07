cbde51e6380b14519893827a924bef2104da6c10
dLswStudy <devLswStudy@gmail.com>
2024-02-07 23:21:03 +0900

    20. 예외 처리와 메시지 출력

--- src/store/movie.js
+++ src/store/movie.js
@@ -6,7 +6,8 @@ const store = new Store({
     page: 1,
     pageMax: 1,
     movies: [],
-    loading: false
+    loading: false,
+    message: 'Search for the movie title!'
 })
 
 export {store as movieStore};
@@ -15,13 +16,24 @@ export const searchMovies = async page => {
     store.state.page = page
     if (page === 1) {
         store.state.movies = []
+        store.state.message = ''
     }
-    const res = await fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}`);
-    const {Search, totalResults } = await res.json();
-    store.state.movies = [
-        ...store.state.movies,
-        ...Search
-    ]
-    store.state.pageMax = Math.ceil(Number(totalResults) / 10)
-    store.state.loading = false
+    try {
+        const res = await fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${store.state.searchText}&page=${page}`);
+        const {Search, totalResults, Response, Error} = await res.json();
+        if (Response === 'True') {
+            store.state.movies = [
+                ...store.state.movies,
+                ...Search
+            ]
+            store.state.pageMax = Math.ceil(Number(totalResults) / 10)
+        } else {
+            store.state.message = Error
+        }
+    } catch (error) {
+        console.log('searchMovies error:', error)
+    } finally {
+        store.state.loading = false
+    }
+
 }
